ca59ae967131cdd38587c142a244d2965d62e24f
lee Jeong Eun <wjd7dms7@naver.com>
2022-11-15 15:24:02 +0900

    배열 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { useRef } from 'react';
-import './App.css';
-import Counter from './Counter';
-import Hello from './Hello';
-import InputSample from './InputSample';
-import UserList from './UserList';
-import Wrapper from './Wrapper';
-
+import { useRef, useState } from "react";
+import "./App.css";
+// import Counter from "./Counter";
+// import Hello from "./Hello";
+// import InputSample from "./InputSample";
+import UserList from "./UserList";
+// import Wrapper from "./Wrapper";
+import CreateUser from "./CreateUser";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const {username, email} = inputs;
+
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: "velopert",
@@ -24,18 +39,42 @@ function App() {
       username: "liz",
       email: "liz@example.com",
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
-  const onChange= () => {
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    // setUsers([...users, user]) // 1. 스프레드 연산자(배열 복사)를 이용하여 배열 추가
+    setUsers(users.concat(user)); // 2. concat을 이용하여 기존의 배열을 수정하지 않고, 새로운 원소가 추가된 새로운 배열을 만듦
 
-
+    setInputs({
+      username: '',
+      email: ''
+    })
     nextId.current += 1;
-  } 
+  };
 
+
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  }
 
   return (
-    <UserList users={users} />
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} />
+    </>
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -1,18 +1,24 @@
 import React from "react";
 
-function User({ user }) {
+function User({ user, onRemove }) {
   return (
     <div>
       <b>{user.username}</b> <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-export default function UserList({ users }) {
+
+export default function UserList({ users, onRemove }) {
   return (
     <div>
-      {users.map((user) => (
-        <User user={user} key={user.id} />
+      {/* {users.map((user) => (   */}
+      {/* ㄴ 순서 정방향 정렬 */}
+      {users.slice(0).reverse().map((user) => (
+        // 순서 역순 정렬 (새로 추가한것이 위에)
+        // 원본 배열을 건드리지 않고 역순으로 하려면 사본을 만든 다음 반대로 만들고 map을 실행
+        <User user={user} key={user.id} onRemove={onRemove}/>
       ))}
     </div>
   );
