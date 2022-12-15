0c491040dcceab5610ef977abc4317868f833014
lee Jeong Eun <wjd7dms7@naver.com>
2022-12-15 17:05:35 +0900

    18. useCallback 을 사용하여 함수 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import "./App.css";
 // import Counter from "./Counter";
 // import Hello from "./Hello";
@@ -7,80 +7,89 @@ import UserList from "./UserList";
 // import Wrapper from "./Wrapper";
 import CreateUser from "./CreateUser";
 
-
 function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는 중...');
-  return users.filter(user => user.active).length;
+  console.log("활성 사용자 수를 세는 중...");
+  return users.filter((user) => user.active).length;
 }
 
 function App() {
   const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
+    username: "",
+    email: "",
   });
 
-  const {username, email} = inputs;
+  const { username, email } = inputs;
 
-  const onChange = e => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    })
-  }
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
 
   const [users, setUsers] = useState([
     {
       id: 1,
       username: "velopert",
       email: "public.velopert@gmail.com",
-      active: true
+      active: true,
     },
     {
       id: 2,
       username: "tester",
       email: "tester@example.com",
-      active: false
+      active: false,
     },
     {
       id: 3,
       username: "liz",
       email: "liz@example.com",
-      active: false
+      active: false,
     },
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
-      email
+      email,
     };
     // setUsers([...users, user]) // 1. 스프레드 연산자(배열 복사)를 이용하여 배열 추가
     setUsers(users.concat(user)); // 2. concat을 이용하여 기존의 배열을 수정하지 않고, 새로운 원소가 추가된 새로운 배열을 만듦
 
     setInputs({
-      username: '',
-      email: ''
-    })
+      username: "",
+      email: "",
+    });
     nextId.current += 1;
-  };
+  }, [users, username, email]);
 
+  const onRemove = useCallback(
+    (id) => {
+      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id 가 id 인 것을 제거함
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
+  const onToggle = useCallback(
+    (id) => {
+      setUsers(
+        users.map((user) =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
 
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
-  }
-  const onToggle = id => {
-    setUsers(
-      users.map(user => 
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    )
-  }
-  const count = useMemo(() => countActiveUsers(users), [users]) 
+  // const count = countActiveUsers(users)
+  const count = useMemo(() => countActiveUsers(users), [users]);
   // useMemo : 성능최적화를 위해 사용
   // 첫번째 인수에는 함수, 두번째 인수에는 배열을 넣어주면 된다.
   // 두번째 인수에 넣어준 배열의 값이 바뀔때만 함수가 실행된다.
@@ -88,7 +97,7 @@ function App() {
   // users의 값이 바뀔때만 호출하고 싶은데 input에 값을 입력할때에도 불필요하게 호출 되어서 사용한다
   return (
     <>
-      <CreateUser 
+      <CreateUser
         username={username}
         email={email}
         onChange={onChange}
