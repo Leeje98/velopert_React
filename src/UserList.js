7f64eef8b3914b2f6cf6c146d29fa991092be564
lee Jeong Eun <wjd7dms7@naver.com>
2022-12-15 16:12:48 +0900

    active props생성해서 토글기능 이용하여 클릭 시 컬러 바뀜

--- src/UserList.js
+++ src/UserList.js
@@ -1,29 +1,41 @@
 import React, { useEffect } from "react";
 
-function User({ user, onRemove }) {
-
+function User({ user, onRemove, onToggle }) {
   useEffect(() => {
-    console.log(user)
-  })
+    console.log(user);
+  });
 
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>&nbsp;&nbsp;
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      <span>({user.email})</span>&nbsp;&nbsp;
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-
-export default function UserList({ users, onRemove }) {
+export default function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
-      {users.map((user) => (  
-      // {/* ㄴ 순서 정방향 정렬 */}
-      // {users.slice(0).reverse().map((user) => (
+      {users.map((user) => (
+        // {/* ㄴ 순서 정방향 정렬 */}
+        // {users.slice(0).reverse().map((user) => (
         // 순서 역순 정렬 (새로 추가한것이 위에)
         // 원본 배열을 건드리지 않고 역순으로 하려면 사본을 만든 다음 반대로 만들고 map을 실행
-        <User user={user} key={user.id} onRemove={onRemove}/>
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
