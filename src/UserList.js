b5f8e7b3872977eb13a6b26b2fe06d91ebc8d420
lee Jeong Eun <wjd7dms7@naver.com>
2022-12-15 15:59:26 +0900

    useEffect 사용하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove }) {
+
+  useEffect(() => {
+    console.log(user)
+  })
+
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b>{user.username}</b> <span>({user.email})</span>&nbsp;&nbsp;
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
@@ -13,9 +18,9 @@ function User({ user, onRemove }) {
 export default function UserList({ users, onRemove }) {
   return (
     <div>
-      {/* {users.map((user) => (   */}
-      {/* ㄴ 순서 정방향 정렬 */}
-      {users.slice(0).reverse().map((user) => (
+      {users.map((user) => (  
+      // {/* ㄴ 순서 정방향 정렬 */}
+      // {users.slice(0).reverse().map((user) => (
         // 순서 역순 정렬 (새로 추가한것이 위에)
         // 원본 배열을 건드리지 않고 역순으로 하려면 사본을 만든 다음 반대로 만들고 map을 실행
         <User user={user} key={user.id} onRemove={onRemove}/>
