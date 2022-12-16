ae3d9c0337ab2b034f38860dc4ed1a2b485ed4a5
lee Jeong Eun <wjd7dms7@naver.com>
2022-12-16 17:46:56 +0900

    20. useReducer 를 사용하여 상태 업데이트 로직 분리하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useReducer, useRef, useState } from "react";
 import "./App.css";
 // import Counter from "./Counter";
 // import Hello from "./Hello";
@@ -12,24 +12,12 @@ function countActiveUsers(users) {
   return users.filter((user) => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
+const initialState = {
+  inputs: {
     username: "",
     email: "",
-  });
-
-  const { username, email } = inputs;
-
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
-      setInputs(inputs => ({
-        ...inputs,
-        [name]: value,
-      }));
-    }, []);
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: "velopert",
@@ -48,47 +36,84 @@ function App() {
       email: "liz@example.com",
       active: false,
     },
-  ]);
+  ],
+};
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      };
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    };
-    // setUsers([...users, user]) // 1. 스프레드 연산자(배열 복사)를 이용하여 배열 추가
-    setUsers(users => users.concat(user)); // 2. concat을 이용하여 기존의 배열을 수정하지 않고, 새로운 원소가 추가된 새로운 배열을 만듦
 
-    setInputs({
-      username: "",
-      email: "",
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    dispatch({
+      type: "CHANGE_INPUT",
+      name,
+      value,
+    });
+  }, []);
+
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: "CREATE_USER",
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
     });
     nextId.current += 1;
   }, [username, email]);
 
-  const onRemove = useCallback(
-    (id) => {
-      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인 것을 제거함
-      setUsers(users => users.filter((user) => user.id !== id));
-    }, []);
-  const onToggle = useCallback(
-    (id) => {
-      setUsers(users =>
-        users.map((user) =>
-          user.id === id ? { ...user, active: !user.active } : user
-        )
-      );
-    }, []);
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    })
+  }, [])
 
-  // const count = countActiveUsers(users)
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    })
+  }, [])
+
   const count = useMemo(() => countActiveUsers(users), [users]);
-  // useMemo : 성능최적화를 위해 사용
-  // 첫번째 인수에는 함수, 두번째 인수에는 배열을 넣어주면 된다.
-  // 두번째 인수에 넣어준 배열의 값이 바뀔때만 함수가 실행된다.
-  // 그렇지 않다면 이전의 값을 재사용한다.
-  // users의 값이 바뀔때만 호출하고 싶은데 input에 값을 입력할때에도 불필요하게 호출 되어서 사용한다
   return (
     <>
       <CreateUser
@@ -97,7 +122,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
       <div>활성사용자 수: {count}</div>
     </>
   );
--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,32 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
+
+
+function reducer(state, active) {
+  switch (active.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT' :
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
 
 export default function Counter() {
 
-  const [number, setNumber] = useState(0);
+  const [number, dispatch] = useReducer(reducer, 0);
 
   const onIncrease = () => {
-    setNumber(prevNumber => prevNumber + 1);
+    // setNumber(prevNumber => prevNumber + 1);
+    dispatch({ type: 'INCREMENT' })
   }
 
   const onDecrease = () => {
-    setNumber(prevNumber => prevNumber - 1);
+    // setNumber(prevNumber => prevNumber - 1);
+    dispatch({ type: 'DECREMENT' })
   }
+ 
 
   return (
     <div>
